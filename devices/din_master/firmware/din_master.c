1ff77513a22bc164e0a91ca60daf766685a07a6b
SOLITON\User <SolitonNew>
2021-03-06 00:22:17 +0200

    Проэкт прошивки для щитового контроллера

---
+++ devices/din_master/firmware/din_master.c
@@ -0,0 +1,16 @@
+/*
+ * din_master.c
+ *
+ * Created: 06.03.2021 0:15:01
+ *  Author: User
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
