ea30ef4f582ffaa5c44aafb2d3e9c13add3d93bd
SolitonNew <adm@soliton.org.ua>
2021-03-10 13:09:37 +0200

    firmware

--- server/app/Library/Firmware.php
+++ server/app/Library/Firmware.php
@@ -9,6 +9,7 @@
 namespace App\Library;
 
 use View;
+use Log;
 
 /**
  * Description of Firmware
@@ -63,6 +64,11 @@ class Firmware {
             $translator = new Script\Translate(new Script\Translators\C($variableNames), $row->DATA);
             $row->DATA_TO_C = $translator->run();
         }
+        
+        // Проверяем наличие директории config
+        if (!file_exists($this->_firmwarePath().'/config')) {
+            mkdir($this->_firmwarePath().'/config');
+        }
 
         // Пакуем в файл config.c
         $fs = new \Illuminate\Filesystem\Filesystem();
@@ -93,24 +99,52 @@ class Firmware {
     public function make(&$outs) {
         $firmwarePath = $this->_firmwarePath();
         
-        $path_c = $firmwarePath.'/'.$this->_project.'.c';
+        // Получаем файлы проекта
+        $xml = simplexml_load_file($firmwarePath.'/'.$this->_project.'.cproj');
+        
+        $files = [];
+        foreach($xml->ItemGroup[0]->Compile as $item) {
+            $file = (string)$item['Include'];
+            if (strpos($file, '.c') === strlen($file) - 2) {
+                $files[] = str_replace('\\', '/', $file);
+            }
+        }
+        
+        // Проверяем наличие или создаем нужные директории
         $release_path = $firmwarePath.'/Release';
         if (!file_exists($release_path)) {
             mkdir($release_path);
-        }        
+        }
         
-        $path_o = $release_path.'/'.$this->_project.'.o';
+        $commands = [];
+        
+        // Собираем комманды для компиляции .c файлов        
+        foreach($files as $file) {
+            $path_c = $firmwarePath.'/'.$file;
+            $path_o = $release_path.'/'.substr($file, 0, strlen($file) - 2).'.o';
+            $commands[] = "avr-gcc -funsigned-char -funsigned-bitfields -Os -fpack-struct -fshort-enums -Wall -c -std=gnu99 -MD -MP -mmcu=$this->_mmcu -o $path_o $path_c";
+        }
+        
+        // Собираем комманды линковки
         $path_elf = $release_path.'/'.$this->_project.'.elf';
+        $files_o = [];
+        foreach($files as $file) {
+            $files_o[] = $release_path.'/'.substr($file, 0, strlen($file) - 2).'.o';
+        }
+        $path_o_all = implode(' ', $files_o);
+        $commands[] = "avr-gcc -o $path_elf $path_o_all -Wl,-Map=\"din_master.map\" -Wl,-lm -mmcu=$this->_mmcu ";
+        
+        // Команда создания прошивки
         $path_hex = $release_path.'/'.$this->_project.'.hex';
+        $commands[] = "avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature  $path_elf $path_hex";
+                    
         
-        $commands = [
-            "avr-gcc -funsigned-char -funsigned-bitfields -Os -fpack-struct -fshort-enums -Wall -c -std=gnu99 -MD -MP -mmcu=$this->_mmcu -o $path_o $path_c",
-            "avr-gcc -o $path_elf $path_o -Wl,-Map=\"din_master.map\" -Wl,-lm -mmcu=$this->_mmcu ",
-            "avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature  $path_elf $path_hex",
-            "avr-size -C --mcu=$this->_mmcu $path_elf"
-        ];
+        // Команда сбора статистики
+        $commands[] = "avr-size -C --mcu=$this->_mmcu $path_elf";
         
+        // Запускаем созданые команды на выполнение
         for($i = 0; $i < count($commands); $i++) {
+            //Log::info($commands[$i]);
             exec($commands[$i].' 2>&1', $outs);
             if (count($outs)) {
                 return ($i == count($commands) - 1);
