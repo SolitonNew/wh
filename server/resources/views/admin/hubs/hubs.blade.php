75ce188838aef529b2c6ab180d2928862ff1a6a2
SolitonNew <adm@soliton.org.ua>
2021-04-13 00:34:09 +0300

    bugs

--- server/resources/views/admin/hubs/hubs.blade.php
+++ server/resources/views/admin/hubs/hubs.blade.php
@@ -13,9 +13,9 @@
 @section('top-menu')
 <div class="nav nav-tabs navbar-top-menu-tab">
     <a class="nav-link @activeSegment(4, 'devices')" 
-        href="{{ route('admin.hub-devices', [$hubID]) }}">@lang('admin/hubs.devices')</a>
+        href="{{ route('admin.hub-devices', $hubID) }}">@lang('admin/hubs.devices')</a>
     <a class="nav-link @activeSegment(4, 'hosts')" 
-        href="{{ route('admin.hub-hosts', [$hubID]) }}">@lang('admin/hubs.hosts')</a>
+        href="{{ route('admin.hub-hosts', $hubID) }}">@lang('admin/hubs.hosts')</a>
 </div>
 @yield('page-top-menu')
 @endsection
@@ -24,7 +24,7 @@
 <div style="display: flex; flex-direction: row; flex-grow: 1;height: 100%;">
     <div class="tree" style="width: 250px;min-width:250px; border-right: 1px solid rgba(0,0,0,0.125);" scroll-store="hubsList">
         @foreach(\App\Http\Models\ControllersModel::whereIsServer(0)->orderBy('id', 'asc')->get() as $row)
-        <a href="{{ route('admin.hubs', [$row->id, $page]) }}"
+        <a href="{{ route('admin.hubs', $row->id).'/'.$page }}"
             class="tree-item {{ $row->id == $hubID ? 'active' : '' }}">
             <div>
                 <div class="" >{{ $row->name }}</div>
