7a1358c3c0e8410d604a8b47a39b9a02ed4a60a6
SolitonNew <adm@soliton.org.ua>
2021-05-01 17:59:38 +0300

    Refactoring

--- server/app/Http/Controllers/Admin/Jurnal/PowerController.php
+++ server/app/Http/Controllers/Admin/Jurnal/PowerController.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\Controller;
 
 class PowerController extends Controller
 {
+    /**
+     * This index route displays statistics on the system's capacity.
+     * 
+     * @return type
+     */
     public function index() 
     {
         return view('admin.jurnal.power.power', [
