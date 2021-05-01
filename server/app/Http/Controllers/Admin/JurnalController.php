1a50e7eefa8e763c570492ca442a9538facc9a47
SolitonNew <adm@soliton.org.ua>
2021-05-01 11:34:59 +0300

    Refactoring

--- server/app/Http/Controllers/Admin/JurnalController.php
+++ server/app/Http/Controllers/Admin/JurnalController.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\Controller;
 
 class JurnalController extends Controller
 {
+    /**
+     * This route redirects to the history page of the journal.
+     * 
+     * @return redirect
+     */
     public function index()
     {
         return redirect(route('admin.jurnal-history'));
