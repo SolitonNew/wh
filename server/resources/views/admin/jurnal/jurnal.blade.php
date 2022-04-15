eee1469b0961353163f59cd22396ad672d007620
SOLITON\User <SolitonNew>
2022-04-15 11:08:48 +0300

    Refactoring

--- server/resources/views/admin/jurnal/jurnal.blade.php
+++ server/resources/views/admin/jurnal/jurnal.blade.php
@@ -6,12 +6,12 @@
 
 @section('top-menu')
 <div class="nav nav-tabs navbar-top-menu-tab">
-    <a class="nav-link @activeSegment(3, 'history')" 
-        href="{{ route('admin.jurnal-history', '') }}">@lang('admin/jurnal.history')</a>
     <a class="nav-link @activeSegment(3, 'daemons')" 
         href="{{ route('admin.jurnal-daemons', '') }}">@lang('admin/jurnal.daemons')</a>
-    {{-- <a class="nav-link @activeSegment(3, 'power')" 
-        href="{{ route('admin.jurnal-power', '') }}">@lang('admin/jurnal.power')</a> --}}
+    <a class="nav-link @activeSegment(3, 'history')" 
+        href="{{ route('admin.jurnal-history', '') }}">@lang('admin/jurnal.history')</a>
+    <!-- <a class="nav-link @activeSegment(3, 'power')" 
+        href="{{ route('admin.jurnal-power', '') }}">@lang('admin/jurnal.power')</a>  -->
 </div>
 @endsection
 
