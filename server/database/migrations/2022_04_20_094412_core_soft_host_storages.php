b97e5f859ab2daef0c73dedc4ccf902e9c102d1e
SOLITON\User <SolitonNew>
2022-04-20 10:19:21 +0300

    Append sohthost storage

---
+++ server/database/migrations/2022_04_20_094412_core_soft_host_storages.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CoreSoftHostStorages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('core_soft_host_storages', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->bigInteger('soft_host_id')->unsigned();
+            $table->longText('data')->nullable();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            
+            $table->foreign('soft_host_id')->references('id')->on('core_soft_hosts')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('core_soft_host_storages');
+    }
+}
