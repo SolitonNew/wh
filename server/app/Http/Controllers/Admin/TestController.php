3ef92aff6d9305055b35e329a47ad19e5f0a3ffb
SOLITON\User <SolitonNew>
2022-05-23 19:02:23 +0300

    test

--- server/app/Http/Controllers/Admin/TestController.php
+++ server/app/Http/Controllers/Admin/TestController.php
@@ -8,18 +8,7 @@ class TestController extends Controller
 {
     public function test()
     {
-        /*\App\Models\ExtApiHost::truncate();
-        \App\Models\OwHost::truncate();
-        \App\Models\I2cHost::truncate(); */
-        
-        $a = [
-            \App\Models\ExtApiHost::count(),
-            \App\Models\OwHost::count(),
-            \App\Models\I2cHost::count(),
-        ];
-        
-        return print_r($a, true);
-        
-        return 'OK';
+        $data = \App\Models\ExtApiHostStorage::orderBy('id')->get();
+        dd($data);
     }
 }
