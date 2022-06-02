b09c2b49872f03f10e5d8a95eb71aa1d5bc15fde
SOLITON\User <SolitonNew>
2022-06-02 10:16:43 +0300

    bug fix

--- server/app/Services/Api/RoomService.php
+++ server/app/Services/Api/RoomService.php
@@ -67,9 +67,7 @@ class RoomService
                 $sql = "select v.created_at, v.value ".
                        "  from core_device_changes v ".
                        " where v.device_id = ".$device->data->id.
-                       "   and v.created_at > (select max(zz.created_at) ".
-                       "                          from core_device_changes zz ".
-                       "                         where zz.device_id = ".$device->data->id.") - interval 3 hour ".
+                       "   and v.created_at > CURRENT_TIMESTAMP() - interval 3 hour".
                        " order by v.id ";
                 
                 $chartData = [];
