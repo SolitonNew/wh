0ca8f9a4a1e460e44788bd16fcfb99f9c6ef6ea9
SOLITON\User <SolitonNew>
2022-08-29 12:44:30 +0300

    Bug fix

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,9 +1,3 @@
 <?php
 
 $router->get('/', ['as' => 'home', 'uses' => 'IndexController@index']);
-
-$router->group(['middleware' => 'auth.terminal'], function () use ($router) {
-    $router->post('broadcasting/auth', function (Illuminate\Http\Request $request) {
-        return Illuminate\Support\Facades\Broadcast::auth($request);
-    });
-});
