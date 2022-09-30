7425b517f9eb057cc22b53459351227e34b7f9fa
Soliton <solitonnew@gmail.com>
2022-09-30 16:58:51 +0300

    Daemons logging

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -37,9 +37,11 @@ class Kernel extends ConsoleKernel
         // If process stopped - to start.
         $schedule->call(function (DaemonManager $daemonManager) {
             foreach(\App\Models\Property::runningDaemons() as $daemon) {
-                if (count($daemonManager->findDaemonPID($daemon)) == 0) {
+                if (count($daemonManager->findDaemonPID($daemon)) !== 0) {
                     try {
+                        Log::channel('daemons')->info("An attempt to launch the '$daemon' demon...");
                         $daemonManager->start($daemon);
+                        Log::channel('daemons')->info("Daemon '$daemon' has been launched");
                     } catch (\Exception $ex) {
                         Log::error($ex->getMessage());
                     }
---
+++ server/config/logging.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['daily'],
+            'ignore_exceptions' => false,
+        ],
+
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+        ],
+
+        'daily' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+            'days' => 14,
+        ],
+
+        'daemons' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/daemons.log'),
+            'level' => 'debug',
+        ],
+    ],
+];
