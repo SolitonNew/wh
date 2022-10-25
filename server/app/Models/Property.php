ad0bef7982a455abfe917784a5f5885363d86cf3
Soliton <solitonnew@gmail.com>
2022-10-25 13:09:41 +0300

    Version

--- server/app/Models/Property.php
+++ server/app/Models/Property.php
@@ -10,7 +10,7 @@ class Property extends Model
     protected $table = 'core_properties';
     public $timestamps = false;
 
-    const VERSION = '2.17.12 alpha';
+    const VERSION = '2.17.13 alpha';
 
     /**
      * @return array
